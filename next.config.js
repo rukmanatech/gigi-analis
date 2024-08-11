794688d7fc0fc1e9502fc3903a0842129b02f0af
Zaki Rukmana <50125675+rukmanatech@users.noreply.github.com>
2024-08-11 17:49:43 +0700

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,11 +1,15 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  output: 'export', // Penting untuk static site generation seperti GitHub Pages
   images: {
-    domains: ['https://rukmanatech.github.io/gigi-analis/'],
+    unoptimized: true, // Diperlukan untuk static exports
+    domains: ['rukmanatech.github.io'], // Hanya nama domain, tanpa protokol atau path
   },
   env: {
     NEXT_PUBLIC_GEMINI_API_KEY: process.env.NEXT_PUBLIC_GEMINI_API_KEY,
   },
+  basePath: '/gigi-analis', // Sesuaikan dengan nama repo Anda
+  assetPrefix: '/gigi-analis/', // Sesuaikan dengan nama repo Anda
   async rewrites() {
     return [
       {
